d6ba07773df9e478e3b1a6eb9f22a8928749930c
lgaume <lorisprog@gmail.com>
2023-11-11 03:06:42 +0100

    Adding infos to operations

--- src/push.c
+++ src/push.c
@@ -6,7 +6,7 @@
 /*   By: lgaume <lgaume@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 02:41:45 by lgaume            #+#    #+#             */
-/*   Updated: 2023/11/11 03:02:12 by lgaume           ###   ########.fr       */
+/*   Updated: 2023/11/11 03:06:13 by lgaume           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 /**
  * 	pa : Push the top node of B on top of A --> [1][2][3][4] [5][6][7] becomes [5][1][2][3][4] [6][7]
+ * 		 Do nothing if B is empty
  * 
  * 	pb : Push the top node of A on top of B --> [1][2][3][4] [5][6][7] becomes [2][3][4] [1][5][6][7]
+ *  	 Do nothing if A is empty
 */
