e859dbc6e0196c1c12a9790b65fd49ffeeeb1d3e
lgaume <lorisprog@gmail.com>
2023-11-14 22:03:26 +0100

    adding sort_stacks

--- src/algorithms/sort_stacks.c
+++ src/algorithms/sort_stacks.c
@@ -6,7 +6,7 @@
 /*   By: lgaume <lgaume@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 08:47:02 by lgaume            #+#    #+#             */
-/*   Updated: 2023/11/14 21:34:07 by lgaume           ###   ########.fr       */
+/*   Updated: 2023/11/14 22:01:15 by lgaume           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,20 +33,56 @@ static void	rotate_both(t_stack_node **a, t_stack_node **b,
 
 static void	move_a_to_b(t_stack_node **a, t_stack_node **b)
 {
-
+	t_stack_node *cheapest;
+	
+	cheapest = get_cheapest(*a);
+	/*if (cheapest->above_median && cheapest->target->above_median)
+		rotate_both(a, b, cheapest, false);
+	else if (!(cheapest->above_median) && (!cheapest->target->above_median))
+		rotate_both(a, b, cheapest, true);*/
+	rotate_both(a, b, cheapest, !(cheapest->above_median && cheapest->target->above_median));
+	prep_for_push(a, cheapest, 'a');
+	prep_for_push(b, cheapest->target, 'b');
+	pb(b, a, false);
 }
 
-static void	move_b_to_a(t_stack_node **a, t_stack_node *b)
+static void	move_b_to_a(t_stack_node **a, t_stack_node **b)
 {
-
+	prep_for_push(a, (*b)->target, 'a');
+	pa(a, b, false);
 }
 
 static void	min_on_top(t_stack_node **a)
 {
-
+	while ((*a)->value != find_max(*a)->value)
+	{
+		if (find_min(*a)->above_median)
+			ra(a, false);
+		else
+			rra(a, false);
+	}
 }
 
 void	sort_stacks(t_stack_node **a, t_stack_node **b)
 {
+	int	len_a;
 
+	len_a = stack_len(*a);
+	if (len_a--  > 3 && !stack_sorted(*a))
+		pb(b, a, false);
+	if (len_a-- > 3 && !stack_sorted(*a))
+		pb(b, a, false);
+	while (len_a-- > 3 && stack_sorted(*a))
+	{
+		init_nodes_a(*a, *b);
+		move_a_to_b(a, b);
+	}
+	sort_three(a);
+	while (*b)
+	{
+		init_nodes_b(*a, *b);
+		move_b_to_a(a, b);
+	}
+	current_index(*a);
+	min_on_top(a);
 }
