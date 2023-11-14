bf1781b16303f21dd6cb6443a8338cecb7cf9e0b
lgaume <lorisprog@gmail.com>
2023-11-14 21:51:45 +0100

    coding init_b_to_a.c

--- src/stacks/init_b_to_a.c
+++ src/stacks/init_b_to_a.c
@@ -6,7 +6,7 @@
 /*   By: lgaume <lgaume@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 08:48:54 by lgaume            #+#    #+#             */
-/*   Updated: 2023/11/14 21:28:55 by lgaume           ###   ########.fr       */
+/*   Updated: 2023/11/14 21:51:27 by lgaume           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,34 @@
 
 static void	set_target_b(t_stack_node *a, t_stack_node *b)
 {
-	
+	t_stack_node	*current_a;
+	t_stack_node	*target;
+	int				best_match_postion;
+
+	while (b)
+	{
+		best_match_postion = INT_MAX;
+		current_a = a;
+		while (current_a)
+		{
+			if (current_a->value > b->value && current_a->value < best_match_postion)
+			{
+				best_match_postion = current_a->value;
+				target = current_a;
+			}
+			current_a = current_a->next;
+		}
+		if (best_match_postion == INT_MAX)
+			b->target = find_min(a);
+		else
+			b->target = target;
+		b = b->next;
+	}
 }
 
 void	init_nodes_b(t_stack_node *a, t_stack_node *b)
 {
-
+	current_position(a);
+	current_position(b);
+	set_target_b(a, b);
 }
