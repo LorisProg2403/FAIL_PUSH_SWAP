ee0008d11e07f33529502a5dd14faf408c077671
lgaume <lorisprog@gmail.com>
2023-11-14 08:46:26 +0100

    uptading header

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: lgaume <lgaume@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 02:41:04 by lgaume            #+#    #+#             */
-/*   Updated: 2023/11/14 08:42:09 by lgaume           ###   ########.fr       */
+/*   Updated: 2023/11/14 08:45:57 by lgaume           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,17 +38,22 @@ int				error_duplicate(t_stack_node *s, int n);
 
 //**Stack initialisation**
 void			init_stack_a(t_stack_node **a, char **av);
+char			**split(char *s, char c);
+
 //**Nodes initialisation**
+//void			init_nodes_a(t_stack_node *a, t_stack_node *b);
+//void			init_nodes_b(t_stack_node *a, t_stack_node *b);
+//void			current_index(t_stack_node *s);
+//void			set_cheapest(t_stack_node *s);
+t_stack_node	*get_cheapest(t_stack_node *s);
+void			prep_for_push(t_stack_node **s, t_stack_node *top, char name);
 
 //**Utils**
-char			**split(char *s, char c);
 t_stack_node	*find_bottom_node(t_stack_node *s);
 bool			stack_sorted(t_stack_node *s);
 int				stack_len(t_stack_node *s);
 t_stack_node	*find_max(t_stack_node *s);
 t_stack_node	*find_min(t_stack_node *s);
-t_stack_node	*get_cheapest(t_stack_node *s);
-void			prep_for_push(t_stack_node **s, t_stack_node *top, char name);
 
 //**Operations**
 void			pa(t_stack_node **a, t_stack_node **b, bool print);
@@ -65,5 +70,6 @@ void			rrr(t_stack_node **A, t_stack_node **B, bool print);
 
 //**Algorithms**
 void			sort_three(t_stack_node **s);
+//void			sort_stacks(t_stack_node **a, t_stack_node **b);
 
 #endif // PUSH_SWAP_H
